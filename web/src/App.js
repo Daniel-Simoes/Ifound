544ccb38a2f30a7517e034ddeb3c86076aea5b9b
daniel.simoes <daniel.simoes@hotmail.com>
2020-01-20 18:53:48 +0000

    finish Web

--- web/src/App.js
+++ web/src/App.js
@@ -1,138 +1,48 @@
 import React, { useState, useEffect} from 'react';
-
+import api from './services/api';
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-function App() {
+import UserForm from './components/UserForm'
+import UserItem from './components/UserItem';
 
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+function App() {
+  const [users, setUsers] = useState([]);
+
+  
+
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) =>{
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
+ async function loadUsers() {
+   const response = await api.get('/users');
+
+   setUsers(response.data);
+ 
+ }
+ loadUsers();
   },[]);
 
-async function handleAddUser(e){
-  e.preventeDefault();
+async function handleAddUser(data){
+ 
+  const response = await api.post('/users', data)
+  
+
+  setUsers([...users, response.data]);
 }
 
   return (
    <div id="app">
     <aside>
      <strong>Sign Up</strong>
-     <form onSubmit={handleAddUser}>
-       <div className='input-block'>
-         <label htmlFor="github_username">Github User</label>
-         <input 
-          name="github_username" 
-          id="github_username" required 
-          value={github_username}
-          onChange={e => setGithubUsername(e.target.value)}
-          />
-       </div>
-
-       <div className='input-block'>
-         <label htmlFor="techs">Technology</label>
-         <input 
-          name="techs" 
-          id="techs" required 
-          value={techs}
-          onChange={e => setTechs(e.target.value)}
-          />
-       </div>
-
-       <div className="input-group">
-       <div className='input-block'>
-         <label htmlFor="latitude">Latitude</label>
-         <input 
-            type='number' 
-            name="latitude" 
-            id="latitude" required 
-            value={latitude} 
-            onChange={e => setLatitude(e.target.value)}
-          />
-       </div>
-
-       <div className='input-block'>
-         <label htmlFor="longitude">Longitude</label>
-         <input  
-          type='number' 
-          name="longitude" 
-          id="longitude" required 
-          value={longitude} 
-          onChange={e => setLongitude(e.target.value)}
-          />
-       </div>
-
-         </div>
-         <button type='submit'>Enter</button>
-     </form>
+     <UserForm onSubmit={handleAddUser}/>
      </aside>
       <main>
         <ul>
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/43343496?s=460&v=4" alt="Daniel Simões"/>
-              <div className='user-info'>
-                <strong>Daniel Simões</strong>
-                <span>NodeJs, ReactJs, React Native</span>
-              </div>
-            </header>
-            <p>Developer in love which por NodeJS, ReactJS and React Native.</p>
-            <a href="https://github.com/Daniel-Simoes">Enter To Github</a>
-          </li>
-
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/43343496?s=460&v=4" alt="Daniel Simões"/>
-              <div className='user-info'>
-                <strong>Daniel Simões</strong>
-                <span>NodeJs, ReactJs, React Native</span>
-              </div>
-            </header>
-            <p>Developer in love which por NodeJS, ReactJS and React Native.</p>
-            <a href="https://github.com/Daniel-Simoes">Enter To Github</a>
-          </li>
-
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/43343496?s=460&v=4" alt="Daniel Simões"/>
-              <div className='user-info'>
-                <strong>Daniel Simões</strong>
-                <span>NodeJs, ReactJs, React Native</span>
-              </div>
-            </header>
-            <p>Developer in love which por NodeJS, ReactJS and React Native.</p>
-            <a href="https://github.com/Daniel-Simoes">Enter To Github</a>
-          </li>
-
-          <li className='dev-item'>
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/43343496?s=460&v=4" alt="Daniel Simões"/>
-              <div className='user-info'>
-                <strong>Daniel Simões</strong>
-                <span>NodeJs, ReactJs, React Native</span>
-              </div>
-            </header>
-            <p>Developer in love which por NodeJS, ReactJS and React Native.</p>
-            <a href="https://github.com/Daniel-Simoes">Enter To Github</a>
-          </li>
+          {users.map(user => (
+            <UserItem key={user._id} user={user} />
+          ))}
         </ul>
       </main>
     </div>
---
+++ web/src/components/UserItem/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles.css';
+
+function UserItem({ user }) {
+    return(
+        <li className='dev-item'>
+            <header>
+              <img src={user.avatar_url} alt={user.name}/>
+              <div className='user-info'>
+                <strong>{user.name}</strong>
+                <span>{user.techs.join(', ')}</span>
+              </div>
+            </header>
+            <p>{user.bio}</p>
+            <a href={`https://github.com/${user.github_username}`}>Enter To Github</a>
+          </li>
+    )
+}
+
+export default UserItem;
