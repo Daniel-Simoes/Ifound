a6a8327a09c3730e679ecddeebcff6112d2b9662
daniel.simoes <daniel.simoes@hotmail.com>
2020-01-21 02:10:11 +0000

    Search's Buttom

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [ currentRegion, setCurrentRegion ] = useState(null);
@@ -33,20 +34,34 @@ function Main({ navigation }) {
 
 
     return (
-    <MapView initialRegion={currentRegion} style={ styles.map }>
-        <Marker coordinate={ { latitude: 53.3223839, longitude: -6.2474962 }}>
-            <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/43343496?s=460&v=4'}}/>
-            <Callout onPress={()=>{
-                navigation.navigate('Profile', { github_username: 'Daniel-Simoes'});
-            }}>
-                 <View style={styles.callout}>
-                    <Text style={styles.userName}>Daniel Simões</Text>
-                    <Text style={styles.userBio}>Apaixonado por NodeJS, ReactJS e React Native.</Text>
-                    <Text style={styles.userTechs}>NodeJS, ReactJS e React Native.</Text>
-                 </View>
-            </Callout>
-        </Marker>
-    </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={ { latitude: 53.3223839, longitude: -6.2474962 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/43343496?s=460&v=4'}}/>
+                    <Callout onPress={()=>{
+                        navigation.navigate('Profile', { github_username: 'Daniel-Simoes'});
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.userName}>Daniel Simões</Text>
+                            <Text style={styles.userBio}>Apaixonado por NodeJS, ReactJS e React Native.</Text>
+                            <Text style={styles.userTechs}>NodeJS, ReactJS e React Native.</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                style={styles.searchInput}
+                placeholder="Search Professionals"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect= {false}
+                />
+                <TouchableOpacity onPress={() =>{}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF"/>
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -75,6 +90,43 @@ const styles = StyleSheet.create({
     userTechs: {
         marginTop: 5,
     },
+    searchForm: {
+        position: 'absolute',
+        bottom: 30,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color:'#333',
+        borderRadius: 25,
+        paddingHorizontal: 15,
+        fontSize: 16,
+        shadowColor: '#333',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 4,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor:'#ec5353',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+        elevation: 20,
+    }
+
+
+
 })
 
 export default Main;
