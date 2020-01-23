ada0dd55f136d685523d894530b4336d3fcf0c00
daniel.simoes <daniel.simoes@hotmail.com>
2020-01-23 00:08:18 +0000

    Socket.io's test/Send information without Request

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -6,7 +6,12 @@ const socket = socketio('http://192.168.43.92:3333', {
 
 function connect(latitude, longitude, techs) {
     socket.io.opts.query = {latitude, longitude, techs,};
+    
     socket.connect();
+
+    socket.on('message', text =>{
+        console.log(text);
+    })
 }
 
 function disconnect() {
